32d9a454f0422e2ed93bc06b825d779c5bb0b090
David Orozco <orozco.davidj02@gmail.com>
2022-02-01 11:57:36 -0500

    fix type anottations for task 4

--- 0x00-python_variable_annotations/4-define_variables.py
+++ 0x00-python_variable_annotations/4-define_variables.py
@@ -3,7 +3,8 @@
 Define and annotate some variables
 """
 
-a = 1
-pi = 3.14
-i_understand_annotations = True
-school = 'Holberton'
+
+a: int = 1
+pi: float = 3.14
+i_understand_annotations: bool = True
+school: str = 'Holberton'
