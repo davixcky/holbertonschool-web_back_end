22abefdaece5d9d897530d33c50391f70d226879
David Orozco <orozco.davidj02@gmail.com>
2022-02-01 12:02:38 -0500

    add file for task 5

---
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""Write a type-annotated function sum_list which takes 
+a list input_list of floats as argument and returns their sum as a float.
+"""
+
+
+def sum_list(input_list: list[float]) -> float:
+     """Return a sum of list (float)"""
+     return sum(input_list)
